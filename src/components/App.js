f163239194866480c0255a9d8d1e04d22ae540b0
rokaya magdi <rokaiamagdi@gmail.com>
2021-02-16 14:58:46 +0200

    [make state available to use in the component] connect between the state of redux store & the component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 class App extends Component {
   state = {}
   render() {
     return (
       <div className="container mt-4">
-        <h2 className="mb-4">Your age : <span className="age">10</span>  </h2>
+        <h2 className="mb-4">Your age : <span className="age">{this.props.age}</span>  </h2>
         <button className="mr-4 p-2 pr-4 pl-4 btn btn-success">Age Up</button>
         <button className="mr-4 p-2 pr-4 pl-4 btn btn-danger">Age Down</button>
       </div>
     );
   }
 }
-
-export default App;
+const mapStateToProps = state => {
+  return {
+    age: state.age
+  };
+};
+export default connect(mapStateToProps)(App);
