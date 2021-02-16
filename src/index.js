0dc00098def762c6f6463e20fc8f9d10ba837a54
rokaya magdi <rokaiamagdi@gmail.com>
2021-02-16 14:57:07 +0200

    create the store & make it available to the App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore, applyMiddleware } from "redux"
+import reducer from './components/reducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
 
+const store = createStore(reducer,)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  , document.getElementById('root'));
 
